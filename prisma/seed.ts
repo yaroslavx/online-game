11c0a1b9dbb618d24680ce6a1ff3acbfe3ad7eb5
yaroslavx <yakupov.yaroslav@icloud.com>
2025-04-05 12:42:00 +0300

    MAINTAIN: Configured prettier

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,23 +1,23 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 async function main() {
-    await prisma.game.create({
-        data: {
-            name: "game-1"
-        }
-    })
-    await prisma.game.create({
-        data: {
-            name: "game-2"
-        }
-    })
+  await prisma.game.create({
+    data: {
+      name: "game-1",
+    },
+  });
+  await prisma.game.create({
+    data: {
+      name: "game-2",
+    },
+  });
 }
 main()
-    .then(async () => {
-        await prisma.$disconnect()
-    })
-    .catch(async (e) => {
-        console.error(e)
-        await prisma.$disconnect()
-        process.exit(1)
-    })
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import {db} from "@/shared/lib/db";
-import {Card, CardTitle} from "@/shared/ui/card";
+import { db } from "@/shared/lib/db";
+import { Card, CardTitle } from "@/shared/ui/card";
 
 export default async function Home() {
-  const games = await db.game.findMany()
+  const games = await db.game.findMany();
 
-  console.log(games)
-
+  console.log(games);
 
   return (
     <div>
