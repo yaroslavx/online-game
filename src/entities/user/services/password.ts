68904053e1e37c9cc9bfae87242663fdff102925
yaroslavx <yakupov.yaroslav@icloud.com>
2025-04-13 08:56:06 +0300

    MAINTAIN: Started implementing user logic

--- src/entities/user/services/password.ts
+++ src/entities/user/services/password.ts
@@ -1,7 +1,23 @@
-export { pbkdf2, randomBytes } from "node:crypto";
+import { pbkdf2, randomBytes } from "node:crypto";
 
-function hashPassword(password: string) {}
+function hashPassword(password: string) {
+  const salt = randomBytes(16).toString("hex");
 
-function comparePassword(password: string, hash: string, salt: string) {}
+  return new Promise<Buffer>((res, rej) =>
+    pbkdf2(password, salt, 1000, 64, "sha512", (error, value) =>
+      error ? rej(error) : res(value),
+    ),
+  ).then((r) => r.toString("hex"));
+}
+
+function comparePassword({
+  password,
+  hash,
+  salt,
+}: {
+  password: string;
+  hash: string;
+  salt: string;
+}) {}
 
 export const passwordService = {};
